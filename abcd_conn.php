e89187dfc40b9c03213b92cf568aa48ff7c129fe
James Hudnall <hud@thehud.com>
2016-03-22 09:48:52 -0700

    Update abcd_conn.php

--- abcd_conn.php
+++ abcd_conn.php
@@ -1,1 +1,15 @@
+<?php
+ require_once 'ABCD_etc.php';
+ require_once 'class.crud.php';
+    /*** a new crud object ***/
+    $crud = new crud();
+
+    /*** The DSN ***/
+    $crud->dsn = "mysql:dbname=abcd;host=localhost";
+
+    /*** MySQL username and password ***/
+    $crud->username = ABCD_USER;
+    $crud->password = ABCD_PASS;
+    $db = new PDO('mysql:host=localhost;dbname='ABCD_DB, ABCD_USER, ABCD_PASS);
+?>
 
