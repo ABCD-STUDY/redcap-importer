0a6130ff3ea995db066577f65a49b0e9f3628751
James Hudnall <hud@thehud.com>
2016-03-28 23:37:52 -0700

    Update abcd_conn.php

--- abcd_conn.php
+++ abcd_conn.php
@@ -1,15 +1,48 @@
 <?php
- require_once 'abcd_etc.php';
- require_once 'class.crud.php';
-    /*** a new crud object ***/
-    $crud = new crud();
+  class Conn{
+    private $host;
+    private $db_name;
+    private $db_username;
+    private $db_password;
+    public $dbh;
+    /**
+     *
+     * Set variables 
+     *
+     */
+     public function __construct( $db_host, $db_name, $db_username, $db_password ){
 
-    /*** The DSN ***/
-    $crud->dsn = "mysql:dbname=abcd;host=localhost";
+            $this->host = $db_host;
+            $this->db_name = $db_name;
+            $this->db_username = $db_username;
+            $this->db_password = $db_password;
+        }
 
-    /*** MySQL username and password ***/
-    $crud->username = ABCD_USER;
-    $crud->password = ABCD_PASS;
-    $db = new PDO('mysql:host=localhost;dbname='ABCD_DB, ABCD_USER, ABCD_PASS);
+        public function connect(){
+            try{                
+                $opt = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+                $this->dbh = new PDO("mysql:host={$this->host}; db_name ={$this->db_name}", 
+                           $this->db_username, $this->db_password, $opt);
+            }
+            catch(PDOException $e){
+                $error = "Error: ".$e->getMessage().'<br />';
+                echo $error;
+                return false;
+            }
+            return true;
+        }
+  }
+    require_once('abcd_etc.php');
+    $db_host = "localhost";
+    $db_name = ABCD_D;
+    $db_username = ABCD_U;
+    $db_password = ABCD_P;
+
+    $db = new Conn($db_host, $db_name, $db_username, $db_password);
+
+    $result = $db->connect();
+
+    if(!$result){
+        echo "DB is not Connected <br />";      
+    } 
 ?>
-
