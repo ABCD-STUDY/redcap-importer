29e8e633e30813838ef218bf60c41771303b58d1
James Hudnall <hud@thehud.com>
2016-04-03 22:00:15 -0700

    Update turn.php
    Modified to read from specified path, it finds the latest file, then runs it though the api importer. Will add code to clean up afterward after testing is finished.

--- turn.php
+++ turn.php
@@ -1,32 +1,27 @@
 <?php
 /*
 *  Turn Cron
-*  Looks for recent files and processes them
+*  Cron Looks for recent files and processes them
 *  Author: James Hudnall
 *  (c) 2016 - ABCD UCSD
 */
-error_reporting(E_ALL); // remove later
-ini_set("display_errors", 1); // remove later
-$dir =  $_SERVER["DOCUMENT_ROOT"] .'/applications/little-man-task/code/sites/';         
-$pattern = '\.(json)$'; // check only for json files       
-$newstamp = 0;            
-$newname = "";
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+require_once 'code/php/config.php';
+require_once 'code/php/class.reader.php';
+$Path = $_SERVER["DOCUMENT_ROOT"] .'/applications/little-man-task/code/sites/';
+$latestTime = 0;
 
-if ($handle = opendir($dir)) {               
-       while (false !== ($fname = readdir($handle)))  {            
-         // Eliminate current directory, parent directory            
-         if (preg_match('^\.{1,2}$^',$fname)) continue;            
-         // Eliminate other pages not in pattern            
-         if (! preg_match($pattern,$fname)) continue;            
-         $timedat = filemtime("$dir/$fname");            
-         if ($timedat > $newstamp) {
-            $newstamp = $timedat;
-            $newname = $fname;
-          }
-         }
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($Path), RecursiveIteratorIterator::CHILD_FIRST);
+foreach ($iterator as $fileinfo) {
+    if ($fileinfo->isFile()) {
+        if ($fileinfo->getMTime() > $latestTime) {
+            $latestTime = $fileinfo->getMTime();
+            $newFile = $fileinfo->getFilename();
+            //print $newFile . " - " . date( "Y/m/d", $latestTime); 
         }
-closedir ($handle);
-
-
-// Show found file (for testing only - remove)        
-print $newname . " - " . date( "Y/m/d", $newstamp); 
+    }
+}
+$source = $Path . $newFile;
+$read = new Reader();
+$data = $read->Parser($source);
