bdd635e6d5ddcb6cb5164c84b8606dc79b679098
James Hudnall <hud@thehud.com>
2016-04-09 16:55:14 -0700

    Update turn.php

--- turn.php
+++ turn.php
@@ -22,8 +22,13 @@ foreach ($iterator as $fileinfo) {
     }
 }
 $source = $Path . $newFile;
+// split the subject and event from the name 
+$c = preg_split('/[_.]/i', $newFile, -1, PREG_SPLIT_OFFSET_CAPTURE);
+print_r($c);
+$event = $c[0];
+$subject = $c[1];
 $read = new Reader();
-if ($read->Parser($source)) {
-    rename($source, $Path . '/read/$newFile');   // move file if successfully processed
+if ($read->Parser($source,$subject, $event)) {
+   // rename($source, $Path . '/read/$newFile');   // move file if successfully processed
 }
 
