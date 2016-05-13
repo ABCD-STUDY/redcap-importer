9affdfbc587b1b6379dd0655a36c2a3275538c34
James Hudnall <hud@thehud.com>
2016-05-12 21:23:03 -0700

    Update create-instrument.php

--- create-instrument.php
+++ create-instrument.php
@@ -1,24 +1,23 @@
-// writes instrument to screen for saving as csv
 <?php 
+// writes instrument to screen for saving as csv
 $out = '"Variable / Field Name","Form Name","Section Header","Field Type","Field Label","Choices, Calculations, OR Slider Labels","Field Note","Text Validation Type OR Show Slider Number","Text Validation Min","Text Validation Max",Identifier?,"Branching Logic (Show field only if...)","Required Field?","Custom Alignment","Question Number (surveys only)","Matrix Group Name","Matrix Ranking?","Field Annotation"
-record_id,little_man_task,,text,"Record ID",,,,,,,,,,,,,
-lmt_site,little_man_task,,text,Site,,,,,,,,,,,,,
-lmt_assessment_date,little_man_task,,text,"Assessment Date",,,,,,,,,,,,,
-lmt_event_name,little_man_task,,text,"Event name",,,,,,,,,,,,,
-lmt_complete,little_man_task,,text,"Task Complete",,,,,,,,,,,,,
-rt_0,little_man_task,,text,"Reaction time  0 ",,,,,,,,,,,,,';
+record_id,little_man_task,,text,"Record ID",,,,,,,,,,,,,'. PHP_EOL.
+'lmt_site,little_man_task,,text,"Site",,,,,,,,,,,,,'. PHP_EOL .
+'lmt_assessment_date,little_man_task,,text,"Assessment Date",,,,,,,,,,,,,'. PHP_EOL .
+'lmt_event_name,little_man_task,,text,"Event name",,,,,,,,,,,,,'. PHP_EOL .
+'lmt_complete,little_man_task,,text,"Task Complete",,,,,,,,,,,,,'. PHP_EOL;
 
 for ($x = 0; $x < 43; $x++) {
     $x = sprintf('%02d', $x);
 
-    $out .= 'lmt_rt_'.$x.',little_man_task,,text,"Reaction time '.$x.'",,,,,,,,,,,,,';
-    $out .= 'lmt_stimulus_'.$x.',little_man_task,,text,"Stimulus '.$x.'",,,,,,,,,,,,,';
-    $out .= 'lmt_key_press_'.$x.',little_man_task,,text,"Key Press '.$x.'",,,,,,,,,,,,,';
-    $out .= 'lmt_trial_type_'.$x.',little_man_task,,text,"Trial Type '.$x.'",,,,,,,,,,,,,';
-    $out .= 'lmt_trial_index_'.$x.',little_man_task,,text,"Trial Index '.$x.'",,,,,,,,,,,,,';
-    $out .= 'lmt_time_elapsed_'.$x.',little_man_task,,text,"Time Elapsed '.$x.'",,,,,,,,,,,,,';
-    $out .= 'lmt_internal_node_id_'.$x.',little_man_task,,text,"Internal Node ID '.$x.'",,,,,,,,,,,,,';
-    $out .= 'lmt_is_data_element_'.$x.',little_man_task,,text,"Is Data Element '.$x.'",,,,,,,,,,,,,';
+$out .= 'lmt_rt_'.$x.',little_man_task,,text,"Reaction time '.$x.'",,,,,,,,,,,,,'. PHP_EOL .
+'lmt_stimulus_'.$x.',little_man_task,,text,"Stimulus '.$x.'",,,,,,,,,,,,,'. PHP_EOL .
+'lmt_key_press_'.$x.',little_man_task,,text,"Key Press '.$x.'",,,,,,,,,,,,,'. PHP_EOL .
+'lmt_trial_type_'.$x.',little_man_task,,text,"Trial Type '.$x.'",,,,,,,,,,,,,'. PHP_EOL .
+'lmt_trial_index_'.$x.',little_man_task,,text,"Trial Index '.$x.'",,,,,,,,,,,,,'. PHP_EOL .
+'lmt_time_elapsed_'.$x.',little_man_task,,text,"Time Elapsed '.$x.'",,,,,,,,,,,,,'. PHP_EOL .
+'lmt_internal_node_id_'.$x.',little_man_task,,text,"Internal Node ID '.$x.'",,,,,,,,,,,,,'. PHP_EOL .
+'lmt_is_data_element_'.$x.',little_man_task,,text,"Is Data Element '.$x.'",,,,,,,,,,,,,'."\r\n";
 
 
 }
@@ -29,3 +28,4 @@ echo $out.'<br/>';
         fclose($file);
  */
 ?>
+
