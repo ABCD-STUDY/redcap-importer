df9c7d3c8f0649f107ad4388c253b7999c52186a
Alex DeCastro <alexdecastro2@gmail.com>
2016-06-03 14:12:03 -0700

    seems to work version

--- class.reader.php
+++ class.reader.php
@@ -9,7 +9,11 @@
 class Reader {
     
     var $project = ''; // member variable to indicate what preset should be used to read values
-    
+
+    function setProject( $project ) {
+       $this->project = $project;
+    }
+
     /*
     * GetSite
     * Returns the Site name from the file
@@ -78,14 +82,35 @@ class Reader {
             $obj = (object) json_decode(file_get_contents($source), true); // cast data to object for processing
             $log = null;
             $send = array();
-            $send['id_redcap'] = $obj->record_id;
+
+	    // 
+	    // copy all global level keys from data
+	    //
+	    $ks = array_keys(get_object_vars($obj));
+	    for ($i = 0; $i < count($ks); $i++) {
+	       if ( $ks[$i] == "data")
+	       	  continue;
+	       if ($ks[$i] == "subject_id")
+	          continue;
+	       if ($ks[$i] == "redcap_event_name")
+	          continue;
+               // copy all that are unique
+	       $send[$ks[$i]] = $obj->{$ks[$i]};
+            }
+	    $send['id_redcap'] = $obj->{"subject_id"};
+	    $send['redcap_event_name'] = $obj->{"redcap_event_name"};
+
+            /*
+	    $send['id_redcap'] = $obj->record_id;
             $send['record_id'] = $obj->record_id;
-            $send[$this->project.'_subject_id'] = $obj->lmt_subjectid;
+            $send[$this->project.'_subject_id'] = $obj[$this->project.'_subject_id'];
             $send[$this->project.'_event_name'] = $obj->redcap_event_name;
             $send['redcap_event_name'] = $obj->redcap_event_name;
             $send[$this->project.'_assessment_date'] = $obj->lmt_assessmentDate;
             $send[$this->project.'_site'] = $obj->lmt_site;
             $send['little_man_task_complete'] = '0';
+	    */
+
             $x = 0;
             // pull data array from object
             $data = $obj->data;
@@ -95,6 +120,9 @@ class Reader {
                     // 						put each line in the table as a row for excel
                     $x = sprintf('%02d', $x);
                     $item = ($key === $this->project.'_stimulus') ? htmlspecialchars($item) : $item; // make sure html characters are encoded for stimulus
+		    if (is_bool($item)) {
+		       $item = ($item) ? "TRUE" : "FALSE";
+                    }
                     $send[$key.'_'.$x] = $item;
                     $k = $key.'_'.$x;
                     if (!in_array($k, $fields)) print '<br><font style="color: red; font-size: 15pt; font-style: italic;">Field not Defined: '.$k.'</font><br>'; // field not in instrument
