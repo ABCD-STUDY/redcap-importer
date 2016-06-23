2217954b77d92625404ebddbd9ef0d19c3c3b304
James Hudnall <hud@thehud.com>
2016-06-23 14:53:28 -0700

    Add files via upload
    Updates for 6/23/16 fixing some issues

--- turn.php
+++ turn.php
@@ -7,28 +7,24 @@
 */
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-
 //
 // parameter for this script as used by cron
 //   turn.php <site> <project>
 //   */10 * * * *      php turn.php /var/www/html/applications/little-man-task/ data/UCSD lmt
 //
-
 require_once 'class.reader.php';
 
 if ($argc !== 4) {
    echo ("Usage: provide path <sub-path to data> <project shortcut>\n\n");
    return;
 }
-
 $path      = $_SERVER['argv'][1];
 $site_path = $_SERVER['argv'][2];
 $project   = $_SERVER['argv'][3];
-$api_url   = "https://abcd-rc.ucsd.edu/redcap/api/";
 
+$api_url   = "https://abcd-rc.ucsd.edu/redcap/api/";
 $logfile = $path.DIRECTORY_SEPARATOR.$project.".log";
 file_put_contents($logfile, "called with : ".$path." ".$site_path." ".$project."\n", FILE_APPEND);
-
 $RootPath = $path;
 // path to assessment files
 $Path = $RootPath.DIRECTORY_SEPARATOR.$site_path;
@@ -42,7 +38,6 @@ $ArchivePath = $RootPath.DIRECTORY_SEPARATOR.'data_import_archive';
 if (!is_dir($ArchivePath)) {
    mkdir($ArchivePath, 0700, TRUE);
 }
-
 $latestTime = 0;
 $filestodo = array();
 $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($Path), RecursiveIteratorIterator::CHILD_FIRST);
@@ -58,43 +53,31 @@ foreach ($iterator as $fileinfo) {
     }
 }
 file_put_contents($logfile, "found ".count($filestodo)." files to process\n", FILE_APPEND);
-
 // now start reading the files
 foreach($filestodo as $source) {
     // split the subject and event from the name IF there is any file
     
     if (file_exists($source)) {
         file_put_contents($logfile, "  reading ".$source."\n", FILE_APPEND);
-
-        $read = new Reader();
-	
+        $read = new Reader($source);	
         $pp = pathinfo($source);
-	// project is supplied as an argument to this function (cron job argument)
-        // $project = substr($pp['filename'], 0, 3);  // set project based on first three characters
-        //echo 'Project: ' . $project . '<p>';
-        // $obj = (array) json_decode(file_get_contents($source), true);
-       
+      
         if ($read->isActive($source)) {
             file_put_contents($logfile, "  file is active: ".$source."\n", FILE_APPEND);
-
             $read->setProject($project);
             $site    = $read->GetSite($source);
-
 	    // use the site information to read the token for this upload
 	    $tokens = json_decode(file_get_contents('/var/www/html/code/php/tokens.json'),TRUE);
 	    if (!isset($tokens[$site])) {
-               file_put_contents($logfile, "  ERRRO detecting token for site : ".$site." from file ".$source."\n", FILE_APPEND);	    
+               file_put_contents($logfile, "  ERROR detecting token for site : ".$site." from file ".$source."\n", FILE_APPEND);	    
 	       continue;
 	    }
 	    $api_token = $tokens[$site];
 	    $read->setToken($api_token);
             file_put_contents($logfile, "  use this token to read ".$api_token."\n", FILE_APPEND);
-
             $log = $read->Parser($source);
             if (!isset($log)) {
                 file_put_contents($logfile, "  import no problems ".$source."\n", FILE_APPEND);
-
-
                 // create a location to store error messages and files that could not be loaded
                 $dir = $ArchivePath.DIRECTORY_SEPARATOR.$site;                
                 if ( !is_dir($dir)) {
@@ -103,14 +86,11 @@ foreach($filestodo as $source) {
 		
                 $finfo = pathinfo($source);
                 $pathinfo = pathinfo($source);
-
 		file_put_contents($logfile, "  move file to ".$dir . DIRECTORY_SEPARATOR . $pathinfo['filename'].".".$pathinfo['extension']."\n", FILE_APPEND);
-
                 rename($source, $dir . DIRECTORY_SEPARATOR . $pathinfo['filename'].".".$pathinfo['extension']);
                 // 		move file if successfully processed
             } else {
                 $dir = $FailedPath . DIRECTORY_SEPARATOR . $site;
-
                 file_put_contents($logfile, "  import ERROR ".$source." with ->\"". $log."\"\n", FILE_APPEND);
                 
                 if( !is_dir($dir)) {
@@ -120,10 +100,8 @@ foreach($filestodo as $source) {
                 
                 // create log
 		file_put_contents($dir.DIRECTORY_SEPARATOR.$finfo['filename']."_error.txt", $log);
-
                 // move file to error directory
                 $pathinfo = pathinfo($source);
-
 		file_put_contents($logfile, "  move file to ".$dir . DIRECTORY_SEPARATOR . $pathinfo['filename'].".".$pathinfo['extension'], FILE_APPEND);
 		
                 // move into error directory
